f1cd56fe519a4cd3b7bbf3ae7ef5c8dea2be3022
morr33 <gborieampomah@gmail.com>
2023-06-21 08:32:19 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * print_alpahbet - print the alphabet in lowercase
+ *
+ * Return:void
+ */
+
+void print_alphabet(void);
+{
+	char loa;
+
+	for (loa = 'a'; loa <= 'z'; loa++)
+		_putchar(loa);
+
+		_putchar('\n');
+}
