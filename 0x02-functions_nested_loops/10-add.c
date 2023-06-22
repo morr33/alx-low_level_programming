10b39d9404eb464160def4496e4efe904ef4529d
morr33 <gborieampomah@gmail.com>
2023-06-21 18:08:44 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @a: paremeter a
+ * @b: paremeter b
+ * Description: a function that adds two integers and returns the result.
+ * Return: return result of the output
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
