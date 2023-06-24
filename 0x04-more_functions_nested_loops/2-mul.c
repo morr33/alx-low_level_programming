eceeb5b592ed302e7c057c5cf3c64b5fd6294333
morr33 <gborieampomah@gmail.com>
2023-06-23 22:03:03 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - multiple two integers
+ * @a: first interger paremeter
+ * @b: second integer paremeter
+ * Description:  a function that multiplies two integers
+ * Return: return result of the functions
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
